4df223f96fe674823070dbfa036ae241971dca57
Jeff Overholt <jeffrey.a.overholt@usps.gov>
2022-03-02 16:19:42 -0500

    SQL Queries 1-8

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -14,7 +14,10 @@ on (employee.emp_no = salaries.emp_no);
 -- Number 2 - List first name, last name, and hire date for employees who were hired in 1986.
 -- https://stackoverflow.com/questions/36203613/how-to-extract-year-from-date-in-postgresql
 
-select * from employee
+Select employee.first_name,
+employee.last_name,
+employee.hire_date
+from employee
 where date_part('year', hire_date) = 1986;
 
 -- Number 3 - List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
@@ -30,7 +33,7 @@ from dept_manager
 left join departments
 on (dept_manager.dept_no = departments.dept_no)
 left join employee
-on (dept_manager.emp_no = employee.emp_no)
+on (dept_manager.emp_no = employee.emp_no);
 
 -- Number 4 - List the department of each employee with the following information: employee number, last name, first name, and department name.
 -- Go from Employee to Dept_Emp to Departments
@@ -44,7 +47,7 @@ from employee
 inner join dept_emp
 on (employee.emp_no = dept_emp.emp_no)
 inner join departments
-on (departments.dept_no = dept_emp.dept_no)
+on (departments.dept_no = dept_emp.dept_no);
 
 
 -- Number 5 - List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
@@ -54,18 +57,44 @@ on (departments.dept_no = dept_emp.dept_no)
 		-- (select city_id from city where city like 'Z%'));
 
 select first_name, last_name, sex from employee 
-where first_name = 'Hercules' and last_name like 'B%'
-
-
-
-
+where first_name = 'Hercules' and last_name like 'B%';
 
 -- Number 6 - List all employees in the Sales department, including their employee number, last name, first name, and department name.
+-- Employee (emp no, last name, first name) to dept_emp (dept_no) to departments (dept_name = 'sales' (d007))
 
-
+select employee.emp_no,
+employee.last_name,
+employee.first_name,
+dept_emp.dept_no,
+departments.dept_name
+from employee
+left join dept_emp
+on (employee.emp_no = dept_emp.emp_no)
+left join departments
+on (dept_emp.dept_no = departments.dept_no)
+where departments.dept_name = 'Sales';
 
 -- Number 7 - List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- same as number 6, just include 'Development' Department too
+-- Employee (emp no, last name, first name) to dept_emp (dept_no) to departments (dept_name = 'Sales' (d007), 'Development' (d005))
+-- https://www.tutorialspoint.com/postgresql/postgresql_where_clause.htm
 
-
+select employee.emp_no,
+employee.last_name,
+employee.first_name,
+dept_emp.dept_no,
+departments.dept_name
+from employee
+left join dept_emp
+on (employee.emp_no = dept_emp.emp_no)
+left join departments
+on (dept_emp.dept_no = departments.dept_no)
+where departments.dept_name in ('Sales', 'Development');
 
 -- Number 8 - In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+-- Reference SQL Day 3 Module 6 (Data Relationships)
+-- https://www.techonthenet.com/postgresql/order_by.php#:~:text=When%20sorting%20your%20result%20set,last_name%20field%20in%20descending%20order.
+
+select count (*), employee.last_name from employee
+group by last_name
+order by count desc;
